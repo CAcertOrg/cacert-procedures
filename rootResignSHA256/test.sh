820b9761858c0904afde39fb1146b5e2d39c152f
Felix Dörre <felix@dogcraft.de>
2015-08-23 00:44:04 +0200

    upd: condition for creating test data is wrong

--- rootResignSHA256/test.sh
+++ rootResignSHA256/test.sh
@@ -2,7 +2,7 @@
 
 gcc -g main.c -lcrypto -o tool
 
-if [[ -f root.crt ]] ; then
+if [[ ! -f root.crt ]] ; then
     openssl req -new -newkey rsa:1024 -keyout root.key -subj "/CN=bla" -nodes -out root.csr
     openssl x509 -in root.csr -req -signkey root.key -out root.crt
 fi

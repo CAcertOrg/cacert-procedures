9758b00cc3dc2fc38ed3e25e39f4f4cfd10d87f1
Felix Dörre <felix@dogcraft.de>
2016-01-05 22:20:29 +0100

    fix: Update Class 3 Intermediate certificate to allow chaining to re-signed Class 1 Root certificate
    The Class 1 Root and Class 3 Intermediate certificates both contained an Authority Key Identifier extension,
    that made it impossible to change the Class 1 Root certificate's serial. This lead to at least two known issues:
    
    1. Presenting a browser with the re-signed root, with the same issuer/serial combination
    causes the browser to reject that certificate.
    
    2. Changing the Serial Number, but keeping with the AKI X.509v3 extension causes some
    software (noticed with Firefox) to (completely by spec) to try building a chain that eventually
    should reach the old Class 1 Root. This never happens, as although the re-signed Class 1 Root
    is a valid signer of itself, it is not considered its issuer - causing an infinite chain to be built.
    
    To allow the re-signed Class 1 Root to be considered the issuer of both itself and the
    Class 3 Intermediate certificates the Authority Key Information should only contain the
    Issuer Public Key Information part of the Authority Key Information, while skipping the
    Directory Name and Serial details. We thus need to remove that part of the extension.

--- rootResignSHA256/main.c
+++ rootResignSHA256/main.c
@@ -29,24 +29,34 @@ int add_ext( X509 *issuer, X509 *subj, int nid, const char* value ) {
 
 int main(int argc, char *argv[]){
     BIO *root = BIO_new_file("root.crt", "r");
+    BIO *inter = BIO_new_file("class3.crt", "r");
     BIO *rootkey = BIO_new_file("root.key", "r");
-    BIO *out = BIO_new_file("root_256.crt", "w");
+    BIO *outRoot = BIO_new_file("root_256.crt", "w");
+    BIO *outInter = BIO_new_file("class3_new.crt", "w");
 
-    X509 *cert = PEM_read_bio_X509(root, NULL, 0, NULL);
+    X509 *rootCert = PEM_read_bio_X509(root, NULL, 0, NULL);
+    BIO_free(root);
     EVP_PKEY *pkey = PEM_read_bio_PrivateKey(rootkey, NULL, 0, NULL);
-    if (!cert || !pkey){
+    BIO_free(rootkey);
+    if (!rootCert || !pkey){
         printf("Reading the keys failed\n");
         return -1;
     }
-    if( !add_ext( cert, cert, NID_crl_distribution_points, "URI:http://crl.cacert.org/revoke.crl" ) ){
+
+    // Modifying the Root cert
+    if( !add_ext( rootCert, rootCert, NID_crl_distribution_points, "URI:http://crl.cacert.org/revoke.crl" ) ){
         printf("Error while adding extension\n");
         return -1;
     }
-    if( !add_ext( cert, cert, NID_netscape_ca_revocation_url, "URI:http://crl.cacert.org/revoke.crl" ) ){
+    if( !add_ext( rootCert, rootCert, NID_netscape_ca_revocation_url, "URI:http://crl.cacert.org/revoke.crl" ) ){
         printf("Error while adding extension\n");
         return -1;
     }
-    if( !add_ext( cert, cert, NID_info_access, "OCSP;URI:http://ocsp.cacert.org" ) ){
+    if( !add_ext( rootCert, rootCert, NID_info_access, "OCSP;URI:http://ocsp.cacert.org" ) ){
+        printf("Error while adding extension\n");
+        return -1;
+    }
+    if( !add_ext( rootCert, rootCert, NID_authority_key_identifier, "keyid" ) ){
         printf("Error while adding extension\n");
         return -1;
     }
@@ -56,17 +66,45 @@ int main(int argc, char *argv[]){
         printf("Error while setting Bignum\n");
         return -1;
     }
-    BN_to_ASN1_INTEGER( bn , cert->cert_info->serialNumber );
+    BN_to_ASN1_INTEGER( bn , rootCert->cert_info->serialNumber );
     BN_free(bn);
+    // End Modifying the Root
 
-    X509_sign(cert, pkey, EVP_sha256());
 
-    PEM_write_bio_X509(out, cert);
-    
-    BIO_free(root);
-    BIO_free(rootkey);
-    BIO_free(out);
-    X509_free(cert);
+    X509_sign(rootCert, pkey, EVP_sha256());
+    PEM_write_bio_X509(outRoot, rootCert);
+    BIO_free(outRoot);
+
+    printf("Root done, now to intermediate\n");
+
+    X509 *interCert = PEM_read_bio_X509(inter, NULL, 0, NULL);
+    BIO_free(inter);
+    if (!interCert){
+        printf("Reading the Intermediate Certificate\n");
+        return -1;
+    }
+
+    // Begin Modifying the Intermediate
+    if( !add_ext( rootCert, interCert, NID_authority_key_identifier, "keyid" ) ){
+        printf("Error while adding extension\n");
+        return -1;
+    }
+    BIGNUM *bnInter = BN_new();
+
+    if(BN_hex2bn(&bnInter, "0e") == 0) {
+        printf("Error while setting Bignum\n");
+        return -1;
+    }
+    BN_to_ASN1_INTEGER( bnInter , interCert->cert_info->serialNumber );
+    BN_free(bnInter);
+    // Begin Modifying the Intermediate
+
+    X509_sign(interCert, pkey, EVP_sha256());
+    PEM_write_bio_X509(outInter, interCert);
+    BIO_free(outInter);
+    X509_free(interCert);
+
+    X509_free(rootCert);
     EVP_PKEY_free(pkey);
 
     ERR_load_crypto_strings();
--- rootResignSHA256/test.sh
+++ rootResignSHA256/test.sh
@@ -4,9 +4,20 @@ gcc -g main.c -lcrypto -o tool
 
 if [[ ! -f root.crt ]] ; then
     openssl req -new -newkey rsa:1024 -keyout root.key -subj "/CN=bla" -nodes -out root.csr
-    openssl x509 -in root.csr -req -signkey root.key -out root.crt
+    openssl x509 -in root.csr -req -signkey root.key -out root.crt -set_serial 0 -extfile <(
+	echo "subjectKeyIdentifier=hash"
+	echo "authorityKeyIdentifier=keyid:always,issuer:always"
+    )
+
+    openssl req -new -newkey rsa:1024 -keyout class3.key -subj "/CN=class3" -nodes -out class3.csr
+    openssl x509 -in class3.csr -req -CA root.crt -CAkey root.key -out class3.crt -set_serial 3 -extfile <(
+	echo "subjectKeyIdentifier=hash"
+	echo "authorityKeyIdentifier=keyid:always,issuer:always"
+    )
 fi
 #valgrind --leak-check=full --show-leak-kinds=all ./tool
 
 ./tool
 diff <(openssl x509 -in root.crt -noout -text) <(openssl x509 -in root_256.crt -noout -text)
+echo "diffs of Class3:"
+diff <(openssl x509 -in class3.crt -noout -text) <(openssl x509 -in class3_new.crt -noout -text)
